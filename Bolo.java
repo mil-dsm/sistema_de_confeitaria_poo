e7e6e4ede1415143b80218253a01ae88e84233eb
Milena <milenadsmesquita@alu.ufc.br>
2025-12-13 13:51:19 -0300

    Sub-classes Bolo e Donut

---
+++ Bolo.java
@@ -0,0 +1,41 @@
+public class Bolo extends Produto {
+    String sabor;
+    String cobertura;
+    char tamanho;
+
+    public Bolo(double precoBase, String validade, int quantidade, String sabor, String cobertura, char tamanho) {
+        super("Bolo", precoBase, validade, quantidade);
+        this.sabor = sabor;
+        this.cobertura = cobertura;
+        this.tamanho = tamanho;
+    }
+    
+    @Override
+    public double calcularPrecoFinal() {
+        double precoFinal = super.precoBase;
+
+        switch (tamanho) {
+            case 'P':
+                precoFinal += 0; 
+                break;
+            case 'M':
+                precoFinal += 15;
+                break;
+            case 'G':
+                precoFinal += 35;
+                break;
+        }
+
+        if(cobertura != null) {
+            precoFinal += 5.0;
+        }
+
+        return (precoFinal);
+    }
+
+    @Override
+    public String toString() {
+        return ("Bolo: [Sabor: " + sabor + ", Cobertura: " + cobertura + ", Tamanho: " + tamanho + "]\n" + super.toString());
+    }
+
+}
---
+++ Donut.java
@@ -0,0 +1,28 @@
+public class Donut extends Produto {
+    private String recheio;
+    private String cobertura;
+    private boolean confete;
+
+    public Donut(double precoBase, String validade, int quantidade, String recheio, String cobertura, boolean confete) {
+        super("Donut", precoBase, validade, quantidade);
+        this.recheio = recheio;
+        this.cobertura = cobertura;
+        this.confete = confete;
+    }
+
+    @Override
+    public double calcularPrecoFinal() {
+        double preco = precoBase;
+        if(recheio != null) preco += 3;
+        if(recheio != null) preco += 2;
+
+        if(recheio != null) preco += 1.5;
+
+        return preco * quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Donut: recheio=" + recheio + ", cobertura=" + cobertura + ", confete=" + confete + "\n" + super.toString();
+    }
+}
