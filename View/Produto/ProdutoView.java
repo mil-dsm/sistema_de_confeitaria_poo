207a6497f926775127098b42a34792f7c637c66e
Milena <milenadsmesquita@alu.ufc.br>
2026-01-13 17:04:49 -0300

    Final

--- View/Produto/ProdutoView.java
+++ View/Produto/ProdutoView.java
@@ -1,58 +1,56 @@
-
 package View.Produto;
-import java.awt.BorderLayout;
-import javax.swing.*;
-//import View.Encomenda.EncomendaView;
 
-// import View.Produto.Listeners.*;
+import javax.swing.*;
+import java.awt.*;
+import View.Encomenda.EncomendaView;
+import View.Produto.Listeners.*;
 
 public class ProdutoView extends JFrame {
-
+    // Atributos de manipulação
+    EncomendaView encomendaAtual;
+    // Botões
     private JButton btnVoltar; //volta pra EncomendaView
-
-   // EncomendaView EncomendaAtual;
-
     //Personalizar Produto
-    JMenuBar menuBar = new JMenuBar();
-    JMenu menuOpcoes = new JMenu("Produtos");
-    JMenuItem btnDoce = new JMenuItem("Doce");
-    JMenuItem btnBolo = new JMenuItem("Bolo");
-    JMenuItem btnDonut = new JMenuItem("Donut");
+    JMenuBar menuBar;
+    JMenu menuOpcoes;
+    JMenuItem btnDoce;
+    JMenuItem btnBolo;
+    JMenuItem btnDonut;
 
-    public ProdutoView(EncomendaView encomendaAtual) { //EncomendaView colocar quando tiver hj :)
+    public ProdutoView(EncomendaView encomendaAtual) {
         setTitle("Menu de Produtos");
-        setSize(400, 500);
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
-
-       // this.EncomendaAtual = encomendaAtual;
         //Layout
         setLayout(new BorderLayout());
 
-        // Botão Voltar
-        btnVoltar = new JButton("Voltar");
-        btnVoltar.setBounds(20, 400, 100, 30);
-        JPanel painelBotaoVoltar = new JPanel();
-        ListenerBtnVoltar l1 = new ListenerBtnVoltar(this);
-        btnVoltar.addActionListener(l1);
-        painelBotaoVoltar.add(btnVoltar);
+        // Inicialização de atributos
+        this.encomendaAtual = encomendaAtual;
 
         // Menu Personalizar Produto
-        JMenuBar menuBar = new JMenuBar();
-        menuBar.add(menuOpcoes);
-        
+        menuBar = new JMenuBar();
+        menuOpcoes = new JMenu("Produtos");
+
         //itens do menu
+        btnDoce = new JMenuItem("Doce");
+        btnBolo = new JMenuItem("Bolo");
+        btnDonut = new JMenuItem("Donut");
+        
         menuOpcoes.add(btnDoce);
         menuOpcoes.add(btnBolo);
         menuOpcoes.add(btnDonut);
-
         
+        menuBar.add(menuOpcoes);
+        setJMenuBar(menuBar);
+        
+        // Botões
         ListenerBtnDoce l2 = new ListenerBtnDoce(this);
         btnDoce.addActionListener(l2);
         ListenerBtnBolo l3 = new ListenerBtnBolo(this);
         btnBolo.addActionListener(l3);
-        // ListenerBtnDonut l4 = new ListenerBtnDonut(this);
-        // btnDonut.addActionListener(l4);
+        ListenerBtnDonut l4 = new ListenerBtnDonut(this);
+        btnDonut.addActionListener(l4);
         
         // Painel central
         JPanel painelCentral = new JPanel(new GridBagLayout());
@@ -68,13 +66,12 @@ public class ProdutoView extends JFrame {
 
         setVisible(true);
     }
-    //getters
-        public JButton getBtnVoltar() { return btnVoltar; }
-        public JMenuItem getBtnDoce() { return btnDoce; }
-        public JMenuItem getBtnBolo() { return btnBolo; }
-        public JMenuItem getBtnDonut() { return btnDonut; }
+    public JButton getBtnVoltar() { return btnVoltar; }
+    public JMenuItem getBtnDoce() { return btnDoce; }
+    public JMenuItem getBtnBolo() { return btnBolo; }
+    public JMenuItem getBtnDonut() { return btnDonut; }
 
-    public static void main(String[] args) {
-        new ProdutoView();
+    public EncomendaView getEncomendaView() {
+        return encomendaAtual;
     }
 }
