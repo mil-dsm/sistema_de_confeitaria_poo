c7f154ade446811e57e4117286bc723c4ed8288e
Trizur <anabeatrizcapistranopereira@gmail.com>
2026-01-13 19:23:26 -0300

    Refactor BoloView layout and button positioning

--- View/Produto/Bolo/BoloView.java
+++ View/Produto/Bolo/BoloView.java
@@ -1,8 +1,12 @@
 package View.Produto.Bolo;
 
-import javax.swing.*;
-import View.Produto.ProdutoView;
 import View.Produto.Bolo.Listeners.*;
+import View.Produto.ProdutoView;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.*;
+
 
 public class BoloView extends JFrame {
     // Atributos para a sincronização
@@ -35,24 +39,46 @@ public class BoloView extends JFrame {
         /* Inicialização de variaveis */
         this.paginaProduto = paginaProduto;
 
-        setLayout(null);
+        setLayout(new BorderLayout(10,10));
 
         // Popups
         popupTamanho = new JPopupMenu();
         popupRecheio = new JPopupMenu();
         popupCobertura = new JPopupMenu();
 
+        //voltar e finalizar
+        JPanel painelInferior = new JPanel(new BorderLayout());
         // Botão Voltar
+        JPanel painelVoltar = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        painelInferior.add(painelVoltar, BorderLayout.WEST);
         btnVoltar = new JButton("Voltar");
-        btnVoltar.setBounds(20, 400, 100, 30);
-        add(btnVoltar);
+       // btnVoltar.setBounds(20, 400, 100, 30);
         ListenerBtnVoltarBolo l1 = new ListenerBtnVoltarBolo(this);
         btnVoltar.addActionListener(l1);
+        painelVoltar.add(btnVoltar);
 
         // botões de personalização
+        JPanel painelOpcoes = new JPanel();
+        painelOpcoes.setLayout(new BoxLayout(painelOpcoes, BoxLayout.Y_AXIS));
         btnTamanho = new JButton("Tamanho ▼");
         btnRecheio = new JButton("Recheio ▼");
         btnCobertura = new JButton("Cobertura ▼");
+
+        Dimension tamanhoBotao = new Dimension(120, 28);
+        Dimension recheioBotao = new Dimension(120, 28);
+        Dimension coberturaBotao = new Dimension(120, 28);
+
+        btnTamanho.setPreferredSize(tamanhoBotao);
+        btnRecheio.setPreferredSize(recheioBotao);
+        btnCobertura.setPreferredSize(coberturaBotao);
+
+        painelOpcoes.add(btnTamanho);
+        painelOpcoes.add(btnRecheio);
+        painelOpcoes.add(btnCobertura);
+
+        add(painelOpcoes, BorderLayout.CENTER);
+
+        
 
         //popup tamanho
         ListenerPopupTamanho lpt = new ListenerPopupTamanho(btnTamanho, popupTamanho);
@@ -89,20 +115,21 @@ public class BoloView extends JFrame {
         cremeCobertura.addActionListener(new ListenerBtnCobertura(this, "Creme"));
 
         //finalizar
+        JPanel painelFinalizar = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         btnFinalizar = new JButton("Finalizar");
-        btnFinalizar.setBounds(200, 400, 100, 30);
-        add(btnFinalizar);
+        painelFinalizar.add(btnFinalizar);
+        //btnFinalizar.setBounds(200, 400, 100, 30);
         ListenerBtnFinalizar l5 = new ListenerBtnFinalizar(this);
         btnFinalizar.addActionListener(l5);
+         painelInferior.add(painelFinalizar, BorderLayout.EAST);
+         add(painelInferior, BorderLayout.SOUTH);
 
-        // Posicionando os botões de personalização
-        btnTamanho.setBounds(20, 50, 100, 30);
-        btnRecheio.setBounds(20, 100, 100, 30);
-        btnCobertura.setBounds(20, 150, 100, 30);
+        // // Posicionando os botões de personalização
+        // btnTamanho.setBounds(20, 50, 100, 30);
+        // btnRecheio.setBounds(20, 100, 100, 30);
+        // btnCobertura.setBounds(20, 150, 100, 30);
+        
 
-        add(btnTamanho);
-        add(btnRecheio);
-        add(btnCobertura);
 
         popupTamanho.add(pequeno);
         popupTamanho.add(medio);
@@ -113,7 +140,8 @@ public class BoloView extends JFrame {
         
         popupRecheio.add(sim);
         popupRecheio.add(nao);
-        
+
+        painelOpcoes.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         setVisible(true);
     }
 
